3373684869a593af273ec0e4a451563164d3fe10
Fred Morris <FredrickCPMorris@hotmail.com>
2023-03-02 13:38:22 -0600

    Added more data to ResourceData.js for general and styling resources.

--- dashboard/src/ResourcData.js
+++ dashboard/src/ResourcData.js
@@ -1,15 +1,177 @@
-const generalRespurces = [
-    {
-        name: "Google Firebase",
-        url: "https://firebase.google.com/"
-        icon: ""
-    }
-]
+const generalResources = [
+  {
+    name: "Google Firebase",
+    url: "https://firebase.google.com/",
+    Descriptions: "",
+  },
+  {
+    name: "FlatIcon",
+    url: "https://www.flaticon.com",
+    Descriptions:
+      "Download Free Icons and Stickers for your projects. Images made by and for designers in PNG, SVG, EPS, PSD and CSS formats.",
+  },
+  {
+    name: "Hero Patterns",
+    url: "https://heropatterns.com/",
+    Descriptions:
+      "A collection of repeatable SVG background patterns for you to use on your web projects.",
+  },
+  {
+    name: "Hereo Icons",
+    url: "https://heroicons.com/",
+    Descriptions:
+      "Beautiful hand-crafted SVG icons, by the makers of Tailwind CSS.",
+  },
+  {
+    name: "Blob Maker",
+    url: "https://www.blobmaker.app/",
+    Descriptions: "Make organic SVG shapes for your next design.",
+  },
+  {
+    name: "haikei",
+    url: "https://haikei.app/",
+    Descriptions: "Generate unique SVG design assets",
+  },
+  {
+    name: "",
+    url: "",
+    Descriptions: "",
+  },
+];
+
+const librariesAndFrameworks = [
+  {
+    name: "Atropos.js",
+    url: "https://atroposjs.com",
+    Descriptions:
+      "Atropos is a lightweight, free and open-source JavaScript library to create stunning touch-friendly 3D parallax hover effects.",
+  },
+  {
+    name: "UI Initiatice",
+    url: "https://uiinitiative.com",
+    Descriptions: "Premium Templates & Plugins",
+  },
+  {
+    name: "Headless UI",
+    url: "https://headlessui.com",
+    Descriptions:
+      "Completely unstyled, fully accessible UI components, designed to integrate beautifully with Tailwind CSS.",
+  },
+  {
+    name: "Ant Desgign",
+    url: "https://ant.design",
+    Descriptions:
+      "Help designers/developers building beautiful products more flexible and working with happiness",
+  },
+  {
+    name: "moment.js",
+    url: "https://momentjs.com/docs/",
+    Descriptions:
+      "Moment.js has been successfully used in millions of projects, and we are happy to have contributed to making date and time better on the web.",
+  },
+  {
+    name: "Grommet",
+    url: "https://v2.grommet.io",
+    Descriptions:
+      "Build responsive and accessible mobile-first projects for the web with an easy to use component library",
+  },
+  {
+    name: "Material UI",
+    url: "https://mui.com",
+    Descriptions:
+      "MUI offers a comprehensive suite of UI tools to help you ship new features faster. Start with Material UI, our fully-loaded component library, or bring your own design system to our production-ready components.",
+  },
+  {
+    name: "Evergreen",
+    url: "https://evergreen.segment.com",
+    Descriptions:
+      "Evergreen is a React UI Framework for building ambitious products on the web. Brought to you by Segment.",
+  },
+  {
+    name: "Blueprint",
+    url: "https://blueprintjs.com",
+    Descriptions: "",
+  },
+  {
+    name: "Bootstrap",
+    url: "https://getbootstrap.com/",
+    Descriptions:
+      "Powerful, extensible, and feature-packed frontend toolkit. Build and customize with Sass, utilize prebuilt grid system and components, and bring projects to life with powerful JavaScript plugins.",
+  },
+  {
+    name: "React Bootstrap",
+    url: "https://react-bootstrap.github.io/",
+    Descriptions: "The most popular front-end framework. Rebuilt for React.",
+  },
+  {
+    name: "Glass UI",
+    url: "https://ui.glass/",
+    Descriptions:
+      "A modern CSS UI library based on the glassmorphism design principles that will help you quickly design and build beautiful websites and applications.",
+  },
+  {
+    name: "",
+    url: "",
+    Descriptions: "",
+  },
+];
 
 const stylingResources = [
-
-]
+  {
+    name: "Google Fonts",
+    url: "https://fonts.google.com",
+    Descriptions: "A React-based UI toolkit for the web.",
+  },
+  {
+    name: "Color Hunt",
+    url: "https://colorhunt.co/",
+    Descriptions: "Discover the newest hand-picked palettes of Color Hunt",
+  },
+  {
+    name: "Smooth Shadow",
+    url: "https://shadows.brumm.af/",
+    Descriptions: "Make a smooth shadow, friend.",
+  },
+  {
+    name: "waves.io",
+    url: "https://getwaves.io/",
+    Descriptions: "Make some waves!",
+  },
+  {
+    name: "Nuemorphism.io",
+    url: "https://neumorphism.io/#e0e0e0",
+    Descriptions: "Generate Soft-UI CSS code",
+  },
+  {
+    name: "UI Verse ",
+    url: "https://uiverse.io/",
+    Descriptions:
+      "Create, share, and use beautiful custom elements made with HTML and CSS.",
+  },
+  {
+    name: "drop-shadow",
+    url: "https://developer.mozilla.org/en-US/docs/Web/CSS/filter-function/drop-shadow",
+    Descriptions:
+      "The drop-shadow() CSS function applies a drop shadow effect to the input image. Its result is a <filter-function>.",
+  },
+  {
+    name: "",
+    url: "",
+    Descriptions: "",
+  },
+];
 
 const learningResources = [
+  {
+    name: "",
+    url: "",
+    Descriptions: "",
+  },
+];
 
-]
+export default {
+  generalResources,
+  learningResources,
+  librariesAndFrameworks,
+  stylingResources,
+};
