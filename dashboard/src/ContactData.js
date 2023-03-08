4ad8b09f765f28808c31a1fc073b59816d6adcf1
Fred Morris <FredrickCPMorris@hotmail.com>
2023-03-08 01:07:01 -0600

    Mapped over Contact Page to improve code scalbility and reusability on the contacts page.

--- dashboard/src/ContactData.js
+++ dashboard/src/ContactData.js
@@ -1,22 +1,35 @@
+import Portfolio from "./Pages/ContactPage/ContactPNGs/PortfolioThumbnail.png";
+import LinkedIn from "./Pages/ContactPage/ContactPNGs/Linked.png";
+import Github from "./Pages/ContactPage/ContactPNGs/Github.png";
+import Resume from "./Pages/ContactPage/ContactPNGs/ResumeThumbnail.png";
+
 const contactResources = [
   {
     imgAlt: "Portolio",
     href: "https://portfolio-v1-mocha.vercel.app/",
     description: "Fred Morris Portfolio",
+    imgSrc: Portfolio,
   },
   {
     imgAlt: "LinkedIn",
     href: "https://www.linkedin.com/in/fredmorrisdeveloper/",
     description: "https://www.linkedin.com",
+    imgSrc: LinkedIn,
   },
   {
-    imgAlt: "Portolio",
-    href: "https://portfolio-v1-mocha.vercel.app/",
-    description: "Fred Morris Portfolio",
+    imgAlt: "Github",
+    href: "https://github.com/Fmorris825",
+    description: "https://www.github.com",
+    imgSrc: Github,
   },
   {
-    imgAlt: "Portolio",
-    href: "https://portfolio-v1-mocha.vercel.app/",
-    description: "Fred Morris Portfolio",
+    imgAlt: "Resumé",
+    href: "https://drive.google.com/file/d/1gL8skql4yXF-OOviznoia--6UlTfQGtC/view?usp=sharing",
+    description: "Fred Morris Resumé",
+    imgSrc: Resume,
   },
 ];
+
+export default {
+  contactResources,
+};
--- dashboard/src/Pages/ContactPage/ContactPage.jsx
+++ dashboard/src/Pages/ContactPage/ContactPage.jsx
@@ -4,6 +4,7 @@ import Github from "./ContactPNGs/Github.png";
 import Portfolio from "./ContactPNGs/PortfolioThumbnail.png";
 import Resume from "./ContactPNGs/ResumeThumbnail.png";
 import "./ContactPage.css";
+import ContactData from "../../ContactData";
 
 const { Meta } = Card;
 
@@ -11,76 +12,26 @@ const ContactPage = () => {
   return (
     <div>
       <Row className="contactContainer Container">
-        <Card
-          hoverable
-          style={{
-            width: 240,
-          }}
-          cover={<img alt="LinkedIn" src={LinkedIn} />}
-        >
-          <Meta
-            title={
-              <a
-                href="https://www.linkedin.com/in/fredmorrisdeveloper/"
-                target="_blank"
-              >
-                Fred's LinkedIn
-              </a>
-            }
-            description="https://www.linkedin.com"
-          />
-        </Card>{" "}
-        <Card
-          hoverable
-          style={{
-            width: 240,
-          }}
-          cover={<img alt="Github" src={Github} />}
-        >
-          <Meta
-            title={
-              <a href="https://github.com/Fmorris825" target="_blank">
-                Fred's Github
-              </a>
-            }
-            description="https://www.github.com"
-          />
-        </Card>{" "}
-        <Card
-          hoverable
-          style={{
-            width: 240,
-          }}
-          cover={<img alt="Portolio" src={Portfolio} />}
-        >
-          <Meta
-            title={
-              <a href="https://portfolio-v1-mocha.vercel.app/" target="_blank">
-                Fred's Portfolio Website
-              </a>
-            }
-            description="Fred Morris Portfolio"
-          />
-        </Card>{" "}
-        <Card
-          hoverable
-          style={{
-            width: 240,
-          }}
-          cover={<img alt="Resumé" src={Resume} />}
-        >
-          <Meta
-            title={
-              <a
-                href="https://drive.google.com/file/d/1gL8skql4yXF-OOviznoia--6UlTfQGtC/view?usp=sharing"
-                target="_blank"
-              >
-                Fred's Resumé
-              </a>
-            }
-            description="Fred Morris Resume"
-          />
-        </Card>{" "}
+        {ContactData.contactResources.map((resource) => {
+          return (
+            <Card
+              hoverable
+              style={{
+                width: 240,
+              }}
+              cover={<img alt={resource.imgAlt} src={resource.imgSrc} />}
+            >
+              <Meta
+                title={
+                  <a href={resource.href} target="_blank">
+                    Fred's LinkedIn
+                  </a>
+                }
+                description={resource.description}
+              />
+            </Card>
+          );
+        })}
       </Row>
     </div>
   );
